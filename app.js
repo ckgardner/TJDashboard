738c3a87dc5c45dad5d0f21a5a144189d9323101
cade.gardner <d00299297@dmail.dixie.edu>
2020-06-01 18:19:41 -0600

    first row

--- app.js
+++ app.js
@@ -9,11 +9,25 @@ var app = new Vue({
         colorMode: 'light',
         switch1: false,
 
+        currentVisitors: "271",
+        currentPercentage: "+0,7",
+        visitorsIn: "68",
+        inPercentage: "-1,7",
+        visitorsOut: "288",
+        outPercentage: "+1",
 
         // all data members for colored themes
         backColor: '#EFF3F9',
+        textColor: '#4F4F4F',
+        containerColor: "#FDFDFF",
         sideNavColor: '#FDFDFF', //'#282C31'
         bisonImg: 'images/bison_orange.svg', //'images/bison_yellow.svg
+        currentVisitorsImg: 'images/current light mode.svg',
+        currentPercentageImg: 'images/increase current.svg',
+        inPercentageImg: 'images/decrease in.svg',
+        outPercentageImg: 'images/increase out.svg',
+        visitorsInImg: 'images/in light mode.svg',
+        visitorsOutImg: 'images/out light mode.svg',
     },
     methods: {
         
@@ -21,14 +35,24 @@ var app = new Vue({
     computed: {
         colorTheme: function() {
             if(!this.switch1){ // light mode
-                this.backColor = "EFF3F9";
+                this.backColor = "#EFF3F9";
+                this.textColor = "#4F4F4F";
                 this.sideNavColor = '#FDFDFF';
+                this.containerColor = "#FDFDFF";
                 this.bisonImg = 'images/bison_orange.svg';
+                this.currentVisitorsImg = 'images/current light mode.svg';
+                this.visitorsInImg = 'images/in light mode.svg';
+                this.visitorsOutImg = 'images/out light mode.svg';
             }
             if(this.switch1){ // dark mode
                 this.backColor = "#212427";
+                this.textColor = "#CACACA";
                 this.sideNavColor = '#282C31';
+                this.containerColor = "#282C31";
                 this.bisonImg = 'images/bison_yellow.svg';
+                this.currentVisitorsImg = 'images/current dark mode.svg';
+                this.visitorsInImg = 'images/in dark mode.svg';
+                this.visitorsOutImg = 'images/out dark mode.svg';
             }
         }
     },
